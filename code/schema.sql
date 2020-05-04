981f20ce8bc8a21e56dfc420bacb6ef3d8eea515
Joseph Om <joeom516@gmail.com>
2020-05-04 13:40:55 -0400

    Upload event schema

--- code/schema.sql
+++ code/schema.sql
@@ -1,4 +1,9 @@
-CREATE TABLE test(
-    a VARCHAR(15),
-    b INT
+CREATE TABLE Event (
+    episode_id INT,
+    event_id INT,
+    state VARCHAR(255),
+    year INT,
+    month_name VARCHAR(20),
+    event_type VARCHAR(255),
+    PRIMARY KEY (episode_id, event_id)
 );
