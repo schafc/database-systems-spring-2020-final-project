14fc65d525c86c2599998df1ff2ae154fe09c05c
Joseph Om <joeom516@gmail.com>
2020-05-04 13:44:05 -0400

    Event schema continued

--- code/schema.sql
+++ code/schema.sql
@@ -5,5 +5,11 @@ CREATE TABLE Event (
     year INT,
     month_name VARCHAR(20),
     event_type VARCHAR(255),
+    begin_date_time DATE,
+    end_date_time DATE, 
+    cz_timezone VARCHAR(10),
+    episode_title VARCHAR(255),
     PRIMARY KEY (episode_id, event_id)
 );
+
+
